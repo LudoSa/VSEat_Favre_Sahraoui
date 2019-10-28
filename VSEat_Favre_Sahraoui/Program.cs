bad615f2d0693ad34a7413e4e1d8a049ef6ed415
LudoSa <ludovic.sahraoui@gmail.com>
2019-10-28 09:59:03 +0100

    Add console Writeline in program.cs

--- VSEat_Favre_Sahraoui/Program.cs
+++ VSEat_Favre_Sahraoui/Program.cs
@@ -7,6 +7,7 @@ namespace VSEat_Favre_Sahraoui
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Salut Github 2");
         }
     }
 }
